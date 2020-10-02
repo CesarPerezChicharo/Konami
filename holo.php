43b1282f3e35ff2220ae9ac3db36bbbc01dbe5b7
CesarPerezChicharo <72279786+CesarPerezChicharo@users.noreply.github.com>
2020-10-02 15:56:26 -0500

    Add files via upload

---
+++ holo.php
@@ -0,0 +1,33 @@
+<button class="btn btn-primary" data-toggle="modal" data-target="#modalInsertar">
+	
+	<span class="fas fa-user-plus">Agregar nuevo</span>
+</button>
+<hr>
+<table class="table table-hover" id="tablaDatos">
+	<thead>
+		<tr>
+			<td>Id persona</td>
+			<td>Nombre</td>
+			<td>Apellido</td>
+			<td>Editar</td>
+			<td>Elimininar</td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td>
+			<button class="btn btn-warning">
+				<span class="fas fa-user-edit"></span>
+			</button>
+		</td>
+		<td>
+			<button class="btn btn-danger">
+				<span class="fas fa-trash-alt"></span>
+			</button>
+		</td>
+		</tr>
+	</tbody>
+</table>
